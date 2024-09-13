a20631ca4b66b248208a2c60498545b5b62f5b5d
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2024-09-13 07:14:40 +0330

    update

--- SiteMCI/mci/InstallUltrapath.sh
+++
@@ -1,9 +0,0 @@
-wget https://satellite.idm.mci.ir/pub/RHEL/Linux/3parties/OceanStor_UltraPath_21.5.0_RHEL.zip
-sed -i 's/7.9/7.6/' /etc/redhat-release
-unzip OceanStor_UltraPath_21.5.0_RHEL.zip
-cd RHEL/
-bash install.sh -f unattend_install.conf
-#sed -i 's/7.6/7.9/' /etc/redhat-release
-#upadmin show vlun
-#restart system
-echo "the server needs to be restarted"
