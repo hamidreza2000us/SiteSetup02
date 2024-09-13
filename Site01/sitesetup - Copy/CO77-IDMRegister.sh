7451b0b2209ad5761dde9073243a7b94c4f3cbb2
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2024-09-13 05:31:39 +0330

    remove

--- Site01/sitesetup - Copy/CO77-IDMRegister.sh
+++
@@ -1,8 +0,0 @@
-if [[ -f ~/sitesetup/variables.sh ]]
-then
-  source ~/sitesetup/variables.sh
-fi
-yum install -y ipa-client 
-ipa-client-install --principal admin --password $IDMPass  --unattended  \
---domain $IDMDomain --enable-dns-updates --all-ip-addresses --mkhomedir \
---automount-location=default  --server $IDMHOSTNAME
